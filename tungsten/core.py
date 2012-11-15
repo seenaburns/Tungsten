aa1d38f7435b80a90562799b41771cfa48f6596c
Seena Burns <seenaburns@github>
2012-11-14 23:24:04 -0800

    core.py: Added Result object

--- tungsten/core.py
+++ tungsten/core.py
@@ -8,29 +8,35 @@ Provides user API and response objects
 """
 
 import requests
-
-appid = ''
+from xml.etree.ElementTree import fromstring, ElementTree
 
 class Tungsten(object):
     def __init__(self, appid):
         self.appid = appid
 
     def query(self, input = ''):
+        # Compose query and get
         payload = {'input': input, 'appid': self.appid}
         r = requests.get("http://api.wolframalpha.com/v2/query", params=payload)
-    
-        return r.text
 
-"""
-_WA_app_id = ''
+        # Exit if request error
+        if r.status_code != 200 or r.encoding != 'utf-8':
+            return None
 
-def app_id(appid = ''):
-    _WA_app_id = appid
+        # Construct Result object from XML Result
+        # ElementTree.fromstring requires byte code, so encode r.text
+        return Result(r.text.encode('utf-8'))
 
-def query(input=''):
-    payload = {'input': input, 'appid': _WA_app_id}
-    r = requests.get("http://api.wolframalpha.com/v2/query", params=payload)
-    print _WA_app_id
-    return r.text
+class Result(object):
+    def __init__(self, xml_result = None):
+        # Construct XML tree
+        self.xml = xml_result
+        self.xml_tree = ElementTree(fromstring(xml_result))
 
-"""
+        # Success from queryresult of xml
+        self.success = self.xml_tree.getroot().get('success') == 'true'
+
+        # Error from the error xml group
+        self.error = None
+        if not self.success:
+            self.error = self.xml_tree.find('error').find('msg').text

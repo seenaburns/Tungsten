c42772e4d43e1f9f782b24b7a8d90986ee84c6d8
Seena Burns <seenaburns@github>
2012-11-15 11:36:21 -0800

    Restructure core.py for robustness
    Changes to Tungsten.query() and Result object to handle errors from
    connection/response with Wolfram Alpha server.
    
    - Tungsten.query() always returns a Result object.
    - Tungsten.query() passes error to Result
    - Result protects ElementTree.fromstring from bad input
    - Result.success and Result.error properties to handle various cases
      that would define a failure

--- tungsten/core.py
+++ tungsten/core.py
@@ -12,33 +12,53 @@ from xml.etree.ElementTree import fromstring, ElementTree
 
 class Tungsten(object):
     def __init__(self, appid):
+        """Create a Tungsten object with a set appid"""
         self.appid = appid
 
     def query(self, input = ''):
-        # Compose query and get
-        payload = {'input': input, 'appid': self.appid}
-        r = requests.get("http://api.wolframalpha.com/v2/query", params=payload)
+        """Query Wolfram Alpha and return a Result object"""
+        # Catch any issues with connecting to Wolfram Alpha API
+        try:
+            payload = {'input': input, 'appid': self.appid}
+            r = requests.get("http://api.wolframalpha.com/v2/query", params=payload)
 
-        # Exit if request error
-        if r.status_code != 200 or r.encoding != 'utf-8':
-            return None
+            # Raise Exception (to be returned as error)
+            if r.status_code != 200:
+                raise Exception('Invalid response status code: %s' % (r.status_code))
+            if r.encoding != 'utf-8':
+                raise Exception('Invalid encoding: %s' % (r.encoding))
 
-        # Construct Result object from XML Result
-        # ElementTree.fromstring requires byte code, so encode r.text
-        return Result(r.text.encode('utf-8'))
+        except Exception, e:
+            return Result(error = e)
+
+        return Result(xml = r.text)
 
 class Result(object):
-    def __init__(self, xml_result = None):
-        # Construct XML tree
-        self.xml_tree = ElementTree(fromstring(xml_result))
+    def __init__(self, xml = '', error = None):
+        # ElementTree.fromstring is fragile
+        #   Requires byte code, so encode into utf-8
+        #   Cannot handle None type, so check if xml exists
+        self.xml_tree = None
+        if xml:
+            self.xml_tree = ElementTree(fromstring(xml.encode('utf-8')))
+
+        # Pass any errors from requesting query along
+        self.error_msg = error
 
     @property
     def success(self):
         # Success from queryresult
-        return self.xml_tree.getroot().get('success') == 'true'
+        if not self.error_msg:
+            return self.xml_tree.getroot().get('success') == 'true'
+        else:
+            return False
 
     @property
     def error(self):
+        # Check for errors from requesting query
+        if self.error_msg:
+            return self.error_msg
+
         # Error from XML group
         error = self.xml_tree.find('error')
         if error is not None:
